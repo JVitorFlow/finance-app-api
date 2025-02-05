d3743e240f1037d108ad33363eefd618e4511d66
João Vitor <joaosilvadev10@gmail.com>
2025-02-05 19:53:40 -0300

    feat(routes): add POST /api/users route for user creation

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import "dotenv/config.js";
 import express from "express";
+import { CreateuserController } from "./src/controllers/create-user.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(process.env.PORT, () => console.log("Listening on port 3000"));
+app.post("/api/users", async (request, response) => {
+  const createuserController = new CreateuserController();
+  const { statusCode, body } = await createuserController.execute(request);
+  response.status(statusCode).json(body);
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`),
+);
