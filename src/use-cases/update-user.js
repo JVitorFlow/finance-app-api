e1964e3622eb20be05902be56c532bc3d6e11a1c
João Vitor <joaosilvadev10@gmail.com>
2025-02-06 14:34:54 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,39 @@
+import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
+import { EmailAlreadyExists } from "../errors/user.js";
+import bcrypt from "bcrypt";
+import PostgresUpdateUserRepository from "../repositories/postgres/update-user";
+
+export class UpdateUserUseCase {
+  async execute(userId, updateUserParams) {
+    if (updateUserParams.email) {
+      const postgresgetUserByEmailRepository =
+        new PostgresGetUserByEmailRepository();
+
+      const userWithProvidedEmail =
+        await postgresgetUserByEmailRepository.execute(updateUserParams.email);
+
+      if (userWithProvidedEmail) {
+        throw new EmailAlreadyExists(updateUserParams.email);
+      }
+    }
+
+    const user = {
+      ...updateUserParams,
+    };
+
+    if (updateUserParams.password) {
+      // criptografar a senha
+      const hashedPassword = await bcrypt.hash(updateUserParams.password, 10);
+      user.password = hashedPassword;
+    }
+
+    // chamar o repositorio para atualizar o usuário
+    const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+    const updatedUser = await postgresUpdateUserRepository.execute(
+      userId,
+      user,
+    );
+
+    return await updatedUser;
+  }
+}
