90fd149bd739d1cbd65fdb0e3f6f56968eea4e03
João Vitor <joaosilvadev10@gmail.com>
2025-02-14 19:04:49 -0300

    refactor(repositories): move index.js from postgres folder to repositories root

---
+++ src/repositories/index.js
@@ -0,0 +1,6 @@
+export * from "./postgres/user/create-user.js";
+export * from "./postgres/user/get-user-by-email.js";
+export * from "./postgres/user/get-user-by-id.js";
+export * from "./postgres/user/update-user.js";
+export * from "./postgres/user/delete-user.js";
+export * from "./transaction/create-transaction.js";
