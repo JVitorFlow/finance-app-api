96778c8231ec3e1a841c49058f063cbcb03955f5
João Vitor <joaosilvadev10@gmail.com>
2025-02-21 12:16:11 -0300

    refactor: add validate required fields helper function

--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -4,3 +4,25 @@ import { badRequest } from "./http.js";
 export const checkIfIdIsValid = (id) => validator.isUUID(id);
 export const invalidIdResponse = () =>
   badRequest({ message: "The provide id is not valid" });
+
+export const checkIfIsString = (value) => typeof value === "string";
+
+export const validateRequiredFields = (params, requiredFields) => {
+  for (const field of requiredFields) {
+    const fieldIsUndefined = params[field] === undefined;
+    const fieldIsEmpty =
+      checkIfIsString(params[field]) &&
+      validator.isEmpty(params[field], { ignore_whitespace: false });
+    if (fieldIsUndefined || fieldIsEmpty) {
+      return {
+        missingField: field,
+        ok: false,
+      };
+    }
+  }
+
+  return {
+    ok: true,
+    missingField: undefined,
+  };
+};
--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -7,6 +7,7 @@ import {
   badRequest,
   serverError,
   created,
+  validateRequiredFields,
 } from "../helpers/index.js";
 
 export class CreateuserController {
@@ -20,10 +21,13 @@ export class CreateuserController {
       // validar a requisição (campos obrigatórios, tamanho de senha e e-mail)
       const requiredFields = ["first_name", "last_name", "email", "password"];
 
-      for (const field of requiredFields) {
-        if (!params[field] || params[field].trim().length === 0) {
-          return badRequest({ message: `Missing param: ${field}` });
-        }
+      const { ok: someRequiredFieldIsMissing, missingField } =
+        validateRequiredFields(params, requiredFields);
+
+      if (!someRequiredFieldIsMissing) {
+        return badRequest({
+          message: `The field "${missingField.missingField}" is required`,
+        });
       }
 
       const passwordIsValid = checkIfPasswordIsValid(params.password);
