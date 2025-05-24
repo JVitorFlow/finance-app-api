2cf540e0dc70e6a8543651f4c2889839fdf10f26
JVitorFlow <joaovitorcsg@gmail.com>
2025-05-24 16:38:49 -0300

    feat: add PostgresGetUserBalanceRepository

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -3,7 +3,9 @@ export * from "./postgres/user/get-user-by-email.js";
 export * from "./postgres/user/get-user-by-id.js";
 export * from "./postgres/user/update-user.js";
 export * from "./postgres/user/delete-user.js";
+
 export * from "./transaction/create-transaction.js";
 export * from "./transaction/get-transactions-by-user-id.js";
 export * from "./transaction/update-transaction.js";
 export * from "./transaction/delete-transaction.js";
+export * from "./postgres/user/get-user-balance.js";
