24afc40a45e64874e16fc89e2238c5d20d2a0cae
JVitorFlow <joaovitorcsg@gmail.com>
2025-05-24 16:41:59 -0300

    feat: add route to get user balance

--- src/factories/controllers/user.js
+++ src/factories/controllers/user.js
@@ -3,6 +3,7 @@ import {
   CreateuserController,
   UpdateUserController,
   DeleteUserController,
+  GetUserBalanceController,
 } from "../../controllers/index.js";
 import {
   PostgresGetUserByIdRepository,
@@ -10,6 +11,7 @@ import {
   PostgresGetUserByEmailRepository,
   PostgresUpdateUserRepository,
   PostgresDeleteUserRepository,
+  PostgresGetUserBalanceRepository,
 } from "../../repositories/index.js";
 import {
   GetUserByIdUseCase,
@@ -17,6 +19,7 @@ import {
   UpdateUserUseCase,
   DeleteUserUseCase,
 } from "../../use-cases/index.js";
+import { GetUserBalanceUseCase } from "../../use-cases/user/get-user-balance.js";
 
 export const makeGetUserByIdController = () => {
   const getUserByIdRepository = new PostgresGetUserByIdRepository();
@@ -52,4 +55,19 @@ export const makeDeleteUserController = () => {
   const deleteUserUseCase = new DeleteUserUseCase(deleteUserRepository);
   const deleteUserController = new DeleteUserController(deleteUserUseCase);
   return deleteUserController;
+};
+
+export const makeGetUserBalanceController = () => {
+  const getUserByIdRepository = new PostgresGetUserByIdRepository();
+  const getUserBalanceRepository = new PostgresGetUserBalanceRepository();
+
+  const getUserBalanceUseCase = new GetUserBalanceUseCase(
+    getUserBalanceRepository,
+    getUserByIdRepository,
+  );
+  const getUserBalanceController = new GetUserBalanceController(
+    getUserBalanceUseCase,
+  );
+
+  return getUserBalanceController;
 };
